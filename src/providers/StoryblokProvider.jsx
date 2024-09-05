82f614d26da4f27db77e0ef85d18961c5e216809
Saina Firooz <saina.firooz@gmail.com>
2024-09-05 22:52:34 +0200

    components added

--- src/providers/StoryblokProvider.jsx
+++ src/providers/StoryblokProvider.jsx
@@ -6,22 +6,26 @@ import Page from "@/components/content-types/Page";
 
 import Teaser from "@/components/nestable/Teaser";
 import RichTextDefault from "@/components/nestable/RichText";
+import Header from "@/components/nestable/Header";
+import Hero from "@/components/nestable/Hero";
+import Grid from "@/components/nestable/Grid";
 
 const components = {
-  "page": Page,
-  "teaser":Teaser,
-  "richtext": RichTextDefault
-}
+  page: Page,
+  teaser: Teaser,
+  richtext: RichTextDefault,
+  header: Header,
+  Hero: Hero,
+  Grid: Grid,
+};
 
 storyblokInit({
   accessToken: StoryblokCMS.TOKEN,
 
   use: [apiPlugin],
-  components
+  components,
 });
 
 export default function StoryblokProvider({ children }) {
-  return (
-    children
-  );
+  return children;
 }
