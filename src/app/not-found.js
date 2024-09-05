ab273f5b5bf5b5e3154b3d18c0ed3047d9f3fbd2
Saina Firooz <saina.firooz@gmail.com>
2024-09-05 22:51:37 +0200

    404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 export default function NotFound() {
-    //Fetch the 404 page from storyblok (this component works as server component aswell)
-    //update this component to render a 404 page
-    return <h1>404 notfound</h1>
+  return (
+    <div className="h-screen flex flex-col justify-center items-center text-center">
+      <h1 className="text-5xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4 text-lg">
+        Oops! The page you're looking for doesn't exist.
+      </p>
+      <Link href="/">
+        <button className="mt-6 px-6 py-3 bg-black text-gray-300 rounded-lg hover:bg-custombg transition-colors">
+          Back to home page
+        </button>
+      </Link>
+    </div>
+  );
 }
