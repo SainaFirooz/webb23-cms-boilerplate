ac06ba4284c91f4f4624917ad09d053a93cfb273
Saina Firooz <saina.firooz@gmail.com>
2024-09-05 23:01:00 +0200

    test

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="h-screen flex flex-col justify-center items-center text-center">
       <h1 className="text-5xl font-bold">404 - Page Not Found</h1>
       <p className="mt-4 text-lg">
-        Oops! The page you're looking for doesn't exist.
+        Oops! The page you&apos;re looking for doesn&apos;t exist.
       </p>
       <Link href="/">
         <button className="mt-6 px-6 py-3 bg-black text-gray-300 rounded-lg hover:bg-custombg transition-colors">
